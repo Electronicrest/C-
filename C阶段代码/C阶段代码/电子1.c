6141ee95c83790a6e0005520ed9ec473ca138d84
Electronicrest <371758487@qq.com>
2019-10-12 20:59:56 +0800

    水仙花

--- C阶段代码/C阶段代码/电子1.c
+++ C阶段代码/C阶段代码/电子1.c
@@ -1,48 +1,50 @@
 
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
-
-void DIA(int n)//打印菱形n为*最多为几个
+#include<math.h>//调用pow次方函数
+void flower(int n)//传入要检测数的上限
 {
-	int i;
-	int j;
-	int k;
-	int p;
-	for ( i = 1; i <= n/2; ++i)//第一块打印上半部分菱形,第一个for循环保证多少行
+	for (int i = 100; i <= n; ++i)//利用循环输入要检测的区间
 	{
-		//下来的两个for循环打印一行的内容,要注意循环出口条件,必须保证它在其余行也能打印
-		for ( j=1; j <=n/2+1-i;++j)//打印空格,j<=n/2+1-i保证空格输出量依次是
-		{							//6  5 4 3 2 1,
-			printf(" ");
-		}
-		for ( k = 1; k <=i+i-1;++k)//打印*,k<=2*i-1保证每行的*按1 3 5 6 9 11输出
-		{
-			printf("*");
-		}
-	
-		printf("\n");
+		int  a = 0;
+		int  b = 0;
+		int  c= 0;
+		a = i % 10;//利用取余取到数字的个位
+		b = (i - a) / 10 % 10;//将数字的各位减去在除以10再取余就可以得到数字的十位
+		c =i / 100;//很好的利用整数相除小数部分会被截断这个特性,获取了数字的百位
+		int sum = pow(a, 3) + pow(b, 3) + pow(c, 3);//pow函数返回值是double型但输入的都是整数,,也就不考虑
+		if (i==sum)
+			printf("%d\t", i);
 	}
-	for (i = 1; i <= n; ++i)
-	{
-		printf("*");
-	}
-	printf("\n");
-	for (j = 1; j <= n/2; ++j)
-	{
-		for (i = 1; i <= j; ++i)
-		{
-			printf(" ");
-		}
-		for (i = 1; i <= n - j - j; ++i)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-	
 }
 int main()
 {
-	DIA(21);//n只能为奇数
+	flower(999);//检测要查看数的范围
 	system("pause");
 }
+//int main()
+//{
+//	 flower(999);
+//	system("pause");
+//}
+//#include <stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	int hun, ten, ind, n;
+//	printf("result is:");
+//	for (n = 100; n<1000; n++)  /*整数的取值范围*/
+//	{
+//		hun = n / 100;
+//		ten = (n - hun * 100) / 10;
+//		ind = n % 10;
+//		if (n == hun*hun*hun + ten*ten*ten + ind*ind*ind)  /*各位上的立方和是否与原数n相等*/
+//			printf("%d  ", n);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
