3f523b0b20d014b95dee874a92d17e0089b003ad
Electronicrest <371758487@qq.com>
2019-10-14 11:49:36 +0800

    猜字谜

--- C阶段代码/C阶段代码/电子1.c
+++ C阶段代码/C阶段代码/电子1.c
@@ -2,49 +2,68 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>//调用pow次方函数
-void flower(int n)//传入要检测数的上限
+#include<stdlib.h>//里面包含rand函数,system函数
+#include<time.h>
+void Game(void);//声明Game函数,告诉编译器存在着这样一个函数返回值为空,没有参数
+void menu(void)//打印菜单,调用Game函数
 {
-	for (int i = 100; i <= n; ++i)//利用循环输入要检测的区间
+	printf("---------------------------------\n");
+	printf("---------------------------------\n");
+	printf("温馨提示,我们的游戏是猜字谜游戏,猜1-100之间的整数\n");
+	printf("            开始游戏     1       \n");
+	printf("            结束游戏     0       \n");
+	printf("---------------------------------\n");
+	printf("---------------------------------\n");
+		int num;
+	printf("          请输入您的选择     ");
+	printf("\n");
+	scanf_s("%d", &num);//获取用户输入选择
+	if (num == 1)
 	{
-		int  a = 0;
-		int  b = 0;
-		int  c= 0;
-		a = i % 10;//利用取余取到数字的个位
-		b = (i - a) / 10 % 10;//将数字的各位减去在除以10再取余就可以得到数字的十位
-		c =i / 100;//很好的利用整数相除小数部分会被截断这个特性,获取了数字的百位
-		int sum = pow(a, 3) + pow(b, 3) + pow(c, 3);//pow函数返回值是double型但输入的都是整数,,也就不考虑
-		if (i==sum)
-			printf("%d\t", i);
+		 Game();
 	}
+	else
+		printf("您选择了退出,再见!\n");
+
+}
+void Game(void)//设置游戏内容
+{
+	
+	
+	int num = rand()% 100 + 1;//设置随机数的范围是1-100
+	int user;
+	printf("下面我们开始猜字谜游戏吧!	ready  go !\n ");
+	printf("请输入您猜的1-100内的整数\n");
+	while (1)
+	{
+		scanf_s("%d", &user);
+		if (user > num)
+			printf("您猜的大了呢\n");
+		else if (user < num)
+			printf("您猜的小了呢\n");
+		else if (user == num)
+		{
+			printf("恭喜你,猜对了,智商堪比牛顿,鼓掌!\n");
+			break;
+		}
+	}
+	printf("你太棒了!!!\n");
 }
 int main()
-{
-	flower(999);//检测要查看数的范围
+{ 
+	srand((unsigned int)time(NULL));//随机数生成器,使程序每打开一次,随机数就会发生变化
+	 menu();//调用menu函数,注意函数调用的格式
 	system("pause");
+	return 0;
 }
-//int main()
-//{
-//	 flower(999);
-//	system("pause");
-//}
-//#include <stdio.h>
-//#include<stdlib.h>
-//int main()
-//{
-//	int hun, ten, ind, n;
-//	printf("result is:");
-//	for (n = 100; n<1000; n++)  /*整数的取值范围*/
-//	{
-//		hun = n / 100;
-//		ten = (n - hun * 100) / 10;
-//		ind = n % 10;
-//		if (n == hun*hun*hun + ten*ten*ten + ind*ind*ind)  /*各位上的立方和是否与原数n相等*/
-//			printf("%d  ", n);
-//	}
-//	printf("\n");
-//	system("pause");
-//	return 0;
-//}
